d26a38b9d0481e59448e4b24e786f919a6fa0e78
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-10-30 14:49:47 +0300

    update

--- UserManagementService/UserManagement/src/main/java/callofproject/dev/usermanagement/UserManagementApplication.java
+++ UserManagementService/UserManagement/src/main/java/callofproject/dev/usermanagement/UserManagementApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("callofproject.dev.repository")
 @EnableJpaRepositories(basePackages = "callofproject.dev.repository")
 @EntityScan(basePackages = "callofproject.dev.repository")
