5d31b7e73c98525aadf5971afc98332396765128
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2024-04-25 19:02:08 +0300

    updated filterandsearch service

--- Services/FilterAndSearchService/src/main/java/callofproject/dev/service/filterandsearch/controller/FilterAndSearchController.java
+++ Services/FilterAndSearchService/src/main/java/callofproject/dev/service/filterandsearch/controller/FilterAndSearchController.java
@@ -2,23 +2,32 @@ package callofproject.dev.service.filterandsearch.controller;
 
 import callofproject.dev.service.filterandsearch.dto.ProjectFilterDTO;
 import callofproject.dev.service.filterandsearch.service.FilterService;
+import callofproject.dev.service.filterandsearch.service.SearchService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/filter")
-public class FilterController
+@RequestMapping("/api/filter-and-search/service")
+public class FilterAndSearchController
 {
     private final FilterService m_filterService;
+    private final SearchService m_searchService;
 
-    public FilterController(FilterService filterService)
+    public FilterAndSearchController(FilterService filterService, SearchService searchService)
     {
         m_filterService = filterService;
+        m_searchService = searchService;
     }
 
-    @GetMapping("/projects")
+    @PostMapping("/filter/projects")
     public ResponseEntity<Object> filterProjects(@RequestBody ProjectFilterDTO filterDTO, @RequestParam("p") int page)
     {
         return ResponseEntity.ok(m_filterService.filterProjects(filterDTO, page));
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<Object> search(@RequestParam("keyword") String keyword, @RequestParam("p") int page)
+    {
+        return ResponseEntity.ok(m_searchService.search(keyword, page));
     }
 }
